f5875a5c13a04c75179a164c1d5fabedf1265e56
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-04-27 20:31:09 +0800

    update

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,9 +1,9 @@
 import { Platform } from "react-native";
 
-// Replace this with your computer's local network IP address when testing on physical devices
-const LOCAL_IP = "192.168.1.100"; // Example IP - replace with your actual IP
+const LOCAL_IP = "192.168.68.124";
 
 const getApiUrl = () => {
+<<<<<<< Updated upstream
   // if (__DEV__) {
   //   // Development environment
   //   if (Platform.OS === "android") {
@@ -17,12 +17,25 @@ const getApiUrl = () => {
   //     return `http://${LOCAL_IP}:8000/api/v1`;
   //   }
   // }
+=======
+  console.log(__DEV__);
 
-  // Production environment
+  if (__DEV__) {
+    // Development environment
+    if (Platform.OS === "android") {
+      return "http://10.0.2.2:8000/api/v1";
+    } else if (Platform.OS === "ios") {
+      return "http://localhost:8000/api/v1";
+    } else {
+      return `http://${LOCAL_IP}:8000/api/v1`;
+    }
+  }
+>>>>>>> Stashed changes
+
   return "https://bias-boutique-backend-production.up.railway.app/api/v1";
 };
 
 export const API_CONFIG = {
   BASE_URL: getApiUrl(),
-  TIMEOUT: 15000,
+  TIMEOUT: 5000,
 };
