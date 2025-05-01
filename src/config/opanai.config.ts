7180bf0052be96a8dc3ff80ca71adf54fea6a10b
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-05-02 02:48:24 +0800

    update

--- src/config/opanai.config.ts
+++ src/config/opanai.config.ts
@@ -1,5 +1,6 @@
 export const OPENAI_CONFIG = {
-  API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
+  API_KEY:
+    "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",
   BASE_URL: "https://api.openai.com/v1",
   TIMEOUT: 5000,
 };
