bc7aa9adbd3ccccbf17bbbbc33e792643bfa7b28
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-05-07 10:13:29 +0800

    updated openai config ts file

--- src/config/opanai.config.ts
+++ src/config/opanai.config.ts
@@ -1,6 +1,5 @@
 export const OPENAI_CONFIG = {
-  API_KEY:
-    "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",
+  API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
   BASE_URL: "https://api.openai.com/v1",
   TIMEOUT: 5000,
 };
